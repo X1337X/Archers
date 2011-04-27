eb8269e681aa284afe5aad8707ca60fa29a0944c
TechGuard <robinvnunen@live.nl>
2011-04-27 11:04:28 -0700

    Edited TechGuard/x1337x/Archers/Arrow/ArrowHandler.java via GitHub

--- TechGuard/x1337x/Archers/Arrow/ArrowHandler.java
+++ TechGuard/x1337x/Archers/Arrow/ArrowHandler.java
@@ -2,6 +2,7 @@ package TechGuard.x1337x.Archers.Arrow;
 
 import net.minecraft.server.EntityArrow;
 
+import org.bukkit.Location;
 import org.bukkit.craftbukkit.entity.CraftArrow;
 import org.bukkit.entity.CreatureType;
 import org.bukkit.entity.Giant;
@@ -11,6 +12,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.entity.EntityDamageByProjectileEvent;
 import org.bukkit.util.Vector;
+
+import TechGuard.x1337x.Archers.Properties;
 /**
  * @author TechGuard
  */
@@ -22,13 +25,17 @@ public class ArrowHandler {
 		if(arrow.material == EnumBowMaterial.FIRE){
 			ea.setFireTicks(300);
 		}
+		if(arrow.material == EnumBowMaterial.STANDARD){
+			arrow.speed = (int)Properties.SPEED;
+		}
 		arrow.world.addEntity((EntityArrow)arrow);
 	}
 
 	public static void onArrowDestroy(EntityDamageByProjectileEvent event){
 		Arrow arrow = (Arrow)((CraftArrow)event.getProjectile()).getHandle();
 
-		event.setDamage(arrow.material.getDamageValue());
+		if(arrow.material == EnumBowMaterial.STANDARD) event.setDamage((int)Properties.DAMAGE);
+		else event.setDamage(arrow.material.getDamageValue());
 		arrow.destroy();
 
 		if(arrow.material == EnumBowMaterial.FIRE){
@@ -48,7 +55,7 @@ public class ArrowHandler {
 					giant.remove();
 				}
 		}
-                if(arrow.material == EnumBowMaterial.PIG){
+        if(arrow.material == EnumBowMaterial.PIG){
 			if(event.getEntity() instanceof Pig){
 				Pig pig = (Pig)event.getEntity();
 				PigZombie pigman = (PigZombie) pig.getWorld().spawnCreature(pig.getLocation(), CreatureType.PIG_ZOMBIE);
@@ -61,10 +68,10 @@ public class ArrowHandler {
 				pig.setHealth(pigman.getHealth());
 				pigman.remove();
 			}
-		}	
+		}
         if(arrow.material == EnumBowMaterial.FLY){
         	org.bukkit.entity.LivingEntity entity = (org.bukkit.entity.LivingEntity)event.getEntity();
 			entity.teleportTo(new Location(entity.getWorld(), entity.getLocation().getX(),entity.getLocation().getY()+20, entity.getLocation().getZ()));
 		}
-}
+	}
 }
