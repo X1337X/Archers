d02b7beabce227664bd8fb26a40b5266cee5f1e4
X1337X <pokemonmasterjack@hotmail.co.uk>
2011-04-27 09:53:48 -0700

    EnumBowMaterial updated

--- TechGuard/x1337x/Archers/Arrow/EnumBowMaterial.java
+++ TechGuard/x1337x/Archers/Arrow/EnumBowMaterial.java
@@ -15,36 +15,37 @@ public enum EnumBowMaterial{
     ZOMBIE(		"Zombie",	0,		new Object[]{ Material.LAPIS_BLOCK }),
     TREE(		"Tree",		0,		new Object[]{ Material.SAPLING }),
     PIG(		"Pig",		0,		new Object[]{ Material.GRASS }),
-    ZEUS(		"Zeus",		20,		new Object[]{ Material.DIAMOND_BLOCK });
-	
+    ZEUS(		"Zeus",		20,		new Object[]{ Material.DIAMOND_BLOCK }),
+    TP("tp",0,new Object[]{Material.WEB}),
+	FLY("Fly",0,new Object[]{Material.WOOL});
 	private String name;
 	private short data;
 	private int damage;
 	private Object[] activate;
-	
+
 	EnumBowMaterial(String name, int damage, Object[] activate){
 		this.name = name;
 		this.data = ArrowHandler.lastData++;
 		this.damage = damage;
 		this.activate = activate;
 	}
-	
+
 	public String getName(){
 		return name;
 	}
-	
+
 	public short getDataValue(){
 		return data;
 	}
-	
+
 	public int getDamageValue(){
 		return damage;
 	}
-	
+
 	public Object[] getBlocks(){
 		return activate;
 	}
-	
+
 	public static EnumBowMaterial fromData(short data){
 		for(EnumBowMaterial material : values()){
 			if(material.data == data){
@@ -53,7 +54,7 @@ public enum EnumBowMaterial{
 		}
 		return STANDARD;
 	}
-	
+
 	public static EnumBowMaterial fromName(String name){
 		for(EnumBowMaterial material : values()){
 			if(material.name.toLowerCase().startsWith(name.toLowerCase())){
