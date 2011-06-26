4061d5cbdd87061bb95405a86100903602cc40ad
FCLight <fclight@hotmail.co.uk>
2011-06-26 20:47:59 +0100

    added dev and recommended src - the begining of recomended builds

---
+++ dev src/TechGuard/x1337x/Archers/Arrow/ArrowHandler.java
@@ -0,0 +1,102 @@
+package TechGuard.x1337x.Archers.Arrow;
+
+import java.util.Random;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.entity.CraftArrow;
+import org.bukkit.entity.CreatureType;
+import org.bukkit.entity.Giant;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Pig;
+import org.bukkit.entity.PigZombie;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
+import org.bukkit.event.entity.EntityDamageByProjectileEvent;
+import org.bukkit.inventory.ItemStack;
+
+import TechGuard.x1337x.Archers.Archers;
+
+public class ArrowHandler
+{
+  public static short lastData = 0;
+
+  public static void onArrowCreate(Player p, Arrow arrow) {
+    try {
+      org.bukkit.entity.Arrow ea = (org.bukkit.entity.Arrow)arrow.getBukkitEntity();
+      arrow.speed = Archers.getSpeed(arrow.material.getName());
+      if (arrow.material == EnumBowMaterial.FIRE) {
+        ea.setFireTicks(300);
+      }
+  
+      arrow.world.addEntity(arrow);
+    }
+    catch (Exception localException) {
+    }
+  }
+
+  public static void onArrowDestroy(EntityDamageByProjectileEvent event) {
+    try {
+      Arrow arrow = (Arrow)((CraftArrow)event.getProjectile()).getHandle();
+event.setDamage(Archers.getDamage(arrow.material.getName()));
+    
+      if (arrow.material == EnumBowMaterial.FIRE) {
+        event.getEntity().setFireTicks(80);
+      }
+      else if (arrow.material == EnumBowMaterial.ZOMBIE) {
+        if ((event.getEntity() instanceof Zombie)) {
+          Zombie zombie = (Zombie)event.getEntity();
+          Giant giant = (Giant)zombie.getWorld().spawnCreature(zombie.getLocation(), CreatureType.GIANT);
+          giant.setHealth(zombie.getHealth());
+          zombie.remove();
+        }
+        else if ((event.getEntity() instanceof Giant)) {
+          Giant giant = (Giant)event.getEntity();
+          Zombie zombie = (Zombie)giant.getWorld().spawnCreature(giant.getLocation(), CreatureType.ZOMBIE);
+          zombie.setHealth(giant.getHealth());
+          giant.remove();
+        }
+      }
+      if (arrow.material == EnumBowMaterial.PIG) {
+        if ((event.getEntity() instanceof Pig)) {
+          Pig pig = (Pig)event.getEntity();
+          PigZombie pigman = (PigZombie)pig.getWorld().spawnCreature(pig.getLocation(), CreatureType.PIG_ZOMBIE);
+          pigman.setHealth(pig.getHealth());
+          pig.remove();
+        }
+        else if ((event.getEntity() instanceof PigZombie)) {
+          PigZombie pigman = (PigZombie)event.getEntity();
+          Pig pig = (Pig)pigman.getWorld().spawnCreature(pigman.getLocation(), CreatureType.PIG);
+          pig.setHealth(pigman.getHealth());
+          pigman.remove();
+        }
+      }
+      if (arrow.material == EnumBowMaterial.FLY) {
+        LivingEntity entity = (LivingEntity)event.getEntity();
+        entity.teleport(new Location(entity.getWorld(), entity.getLocation().getX(), entity.getLocation().getY() + 20.0D, entity.getLocation().getZ()));
+  
+      }
+      if ((arrow.material == EnumBowMaterial.STEAL) && 
+        ((event.getEntity() instanceof Player)))
+      {
+        Player p = (Player)event.getEntity();
+        Random r = new Random();
+        int ran = r.nextInt(17);
+        ItemStack iih = p.getInventory().getItem(ran);
+        Location loc = p.getLocation();
+        Location locnew = new Location(loc.getWorld(), loc.getX() + 10.0D, loc.getY() + 10.0D, loc.getBlockZ());
+        if (iih != null || iih.getType() != Material.AIR) {
+          p.getInventory().remove(iih);
+         Player shooter = (Player) event.getDamager();
+         shooter.getInventory().addItem(iih);
+        }
+
+      }
+
+      
+    }
+    catch (Exception localException)
+    {
+    }
+  }
+}
---
+++ dev src/TechGuard/x1337x/Archers/Properties.java
@@ -0,0 +1,221 @@
+package TechGuard.x1337x.Archers;
+
+import TechGuard.x1337x.Archers.Arrow.EnumBowMaterial;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.config.Configuration;
+
+public class Properties
+{
+	static Archers plugin;
+	public Properties(Archers p){
+		this.plugin = p;
+	}
+  public static HashMap<Short, ArrayList<ItemStack>> ArrowAmmo = new HashMap();
+  public static double DAMAGE;
+  public static double SPEED;
+  static String dir = "plugins/Archers/";
+  private static File ArrowFile;
+  private static File ConfigFile;
+
+  public static void reload()
+  {
+    load();
+  }
+
+  private static void load() {
+    ArrowFile = new File(dir + "arrow.ammo");
+    ConfigFile = new File(dir + "config.txt");
+    makeConfig();
+    checkForArrow();
+
+    loadArrow();
+    loadConfig(new Configuration(ConfigFile));
+  }
+
+  private static void checkForArrow() {
+    try {
+      if (!ArrowFile.exists()) {
+        ArrowFile.getParentFile().mkdirs();
+        ArrowFile.createNewFile();
+        BufferedWriter out = new BufferedWriter(new FileWriter(ArrowFile));
+
+        out.write("#The right order:"); out.newLine();
+        out.write("#  ARROW NAME:ITEM ID,AMOUNT:NEW ITEM ID, AMOUNT, etc. etc."); out.newLine();
+        out.write("#Arrow names:"); out.newLine();
+        for (EnumBowMaterial bow : EnumBowMaterial.values()) {
+          out.write("  " + bow.getName()); out.newLine();
+        }
+        out.write("#Lines witch start with the # symbol, will be ignored!"); out.newLine();
+        out.write(""); out.newLine();
+        out.write("#Normal Arrow"); out.newLine();
+        out.write("Normal:262,1"); out.newLine();
+        out.write(""); out.newLine();
+        out.write("#Ice Arrow"); out.newLine();
+        out.write("Ice:332,1:262,1"); out.newLine();
+        out.write(""); out.newLine();
+        out.write("#Fire Arrow"); out.newLine();
+        out.write("Fire:263,1:262,1"); out.newLine();
+        out.write(""); out.newLine();
+        out.write("#TNT Arrow"); out.newLine();
+        out.write("TNT:289,2:262,1"); out.newLine();
+        out.write(""); out.newLine();
+        out.write("#Thunder Arrow"); out.newLine();
+        out.write("Thunder:331,5:262,1"); out.newLine();
+        out.write(""); out.newLine();
+        out.write("#Monster Arrow"); out.newLine();
+        out.write("Monster:352,2:262,1"); out.newLine();
+        out.write(""); out.newLine();
+        out.write("#Thrice Arrow"); out.newLine();
+        out.write("Thrice:262,3"); out.newLine();
+        out.write(""); out.newLine();
+        out.write("#Zombie Arrow"); out.newLine();
+        out.write("Zombie:295,1:262,1"); out.newLine();
+        out.write(""); out.newLine();
+        out.write("#Tree Arrow"); out.newLine();
+        out.write("Tree:6,1:262,1"); out.newLine();
+        out.write(""); out.newLine();
+        out.write("#Pig Arrow"); out.newLine();
+        out.write("Pig:320,1:262,1"); out.newLine();
+        out.write(""); out.newLine();
+        out.write("#Zeus Arrow"); out.newLine();
+        out.write("Zeus:264,2:262,1"); out.newLine();
+        out.write(""); out.newLine();
+        out.write("#TP Arrow"); out.newLine();
+        out.write("Tp:287,1:262,2"); out.newLine();
+        out.write(""); out.newLine();
+        out.write("#Fly arrow"); out.newLine();
+        out.write("Fly:288,5:262,1");
+        out.write(""); out.newLine();
+        out.newLine();
+        out.write("#Torch arrow"); out.newLine();
+        out.write("Torch:50:1:262,1"); out.newLine();
+        out.write("#web arrow"); out.newLine();
+        out.write("Web:30,4:262,1"); out.newLine();
+        out.write("#Steal arrow"); out.newLine();
+        out.write("Steal:287,1:262,1"); out.newLine();
+        out.write("#Sniper arrow"); out.newLine();
+        out.write("Sniper:348,1:262,2");out.newLine();
+        out.write("#Rocket arrow");out.newLine();
+        out.write("Rocket:46,3:262,5");out.newLine();
+        out.write("#Storm arrow");out.newLine();
+        out.write("Storm:326,1:262,1");
+       
+
+        out.newLine();
+        out.close();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+private static void makeConfig(){
+	try{
+		if(!ConfigFile.exists()){
+			ConfigFile.getParentFile().mkdirs();
+			ConfigFile.createNewFile();
+			BufferedWriter b = new BufferedWriter(new FileWriter(ConfigFile));
+			b.write("#usage is arrowname:damage:speed");
+			b.newLine();
+			b.write("#Do not change the arrowname");
+			b.newLine();
+			b.write("#lines starting with a # will be ignored");
+			b.newLine();
+			b.write("#example making tnt move 20 blocks a second and setting its damage to 20");
+			b.newLine();
+			b.write("#TNT:20:20");
+			b.newLine();
+			b.write("#------------------------------------------------------------------------");
+			b.newLine();
+			b.write("Normal:4:1");
+			b.newLine();
+			b.write("Ice:7:1");
+			b.newLine();
+			b.write("Fire:4:1");
+			b.newLine();
+			b.write("TNT:0:2");
+			b.newLine();
+			b.write("Thunder:0:1");
+			b.newLine();
+			b.write("Monster:0:1");
+			b.newLine();
+			b.write("Thrice:4:1");
+			b.newLine();
+			b.write("Zombie:0:1");
+			b.newLine();
+			b.write("Tree:0:1");
+			b.newLine();
+			b.write("Pig:0:1");
+			b.newLine();
+			b.write("Zeus:20:3");
+			b.newLine();
+			b.write("TP:0:1");
+			b.newLine();
+			b.write("Fly:0:1");
+			b.newLine();
+			b.write("Web:0:1");
+			b.newLine();
+			b.write("Steal:0:1");
+			b.newLine();
+			b.write("Sniper:100:70");
+			b.newLine();
+			b.write("Rocket:0:10");
+			b.newLine();
+			b.write("Storm:0:5");
+			b.close();
+
+           
+
+plugin.sM("Made config.txt sucessfully!");
+		}
+	}catch(Exception e){
+		e.printStackTrace();
+	}
+
+}
+  
+  private static void loadArrow() {
+    try {
+      BufferedReader br = new BufferedReader(new InputStreamReader(new DataInputStream(new FileInputStream(ArrowFile))));
+      String strLine;
+      while ((strLine = br.readLine()) != null)
+      {
+      
+        if ((strLine.startsWith("#")) || (strLine.startsWith(" ")) || (!strLine.contains(":"))) {
+          continue;
+        }
+        String[] split = strLine.split(":");
+        ArrayList list = new ArrayList();
+        for (String s : split) {
+          if (s.contains(",")) {
+            list.add(new ItemStack(Integer.parseInt(s.split(",")[0]), Integer.parseInt(s.split(",")[1])));
+          }
+        }
+        ArrowAmmo.put(Short.valueOf(EnumBowMaterial.fromName(split[0]).getDataValue()), list);
+      }
+      br.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void loadConfig(Configuration config) {
+    try {
+      config.load();
+
+      String tag = "Global.";
+      DAMAGE = config.getDouble(tag + "Normal-Arrow-Damage", 4.0D);
+      SPEED = config.getDouble(tag + "Normal-Arrow-Speed", 1.0D);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
